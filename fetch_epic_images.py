4b605a56694a2714eb3c3daf6309793199ef9af3
Egor Bezborodov <egor.bezborodoff@gmail.com>
2025-05-12 00:00:42 +0300

    Use urlencode for safe query parameter handling in EPIC image requests

--- fetch_epic_images.py
+++ fetch_epic_images.py
@@ -3,13 +3,15 @@ import requests
 import concurrent.futures
 from utils import get_file_extension, download_image
 from dotenv import load_dotenv
+from urllib.parse import urlencode
 
 
 def download_epic_image(item, i, api_key):
     name = item["image"]
     date = item["date"].split()[0]
     year, month, day = date.split('-')
-    img_url = f"https://api.nasa.gov/EPIC/archive/natural/{year}/{month}/{day}/png/{name}.png?api_key={api_key}"
+    query = urlencode({"api_key": api_key})
+    img_url = f"https://api.nasa.gov/EPIC/archive/natural/{year}/{month}/{day}/png/{name}.png?{query}"
 
     filename = os.path.join("epic_images", f"epic_{i + 1}.png")
     download_image(img_url, filename)
